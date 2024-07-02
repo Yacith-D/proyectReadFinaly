0635a9261ae7dde59653c8a45e784ef4587d44be
Yacith-D <martinezyacith@gmail.com>
2024-07-01 23:25:40 -0600

    realizacion de pagina de contacto

--- reactproyect/src/components/contacto/contacto.jsx
+++ reactproyect/src/components/contacto/contacto.jsx
@@ -8,7 +8,16 @@ const Contacto = () => {
       <p className='informacion'>Si quieres información de nuestros productos <br /> o estas interesado en otra aspecto o consulta, <br /> llena nuestros formularios y nos pondremos en contacto contigo</p>
       <p className='gracias'>muchas gracias</p>
      <h5>Sede central: </h5> 
-     <p>eeededede</p>
+     <p className='direccion'>camino a puntarenas 1723-entrada del boli</p>
+     <p className='direccion2'>chacarita puntarenas</p>
+    <h5>TELEFONO:</h5>
+    <p className='tell'>(+506) 7683-9816</p>
+    <h5>Administración</h5>
+    <p>fwdcostarica@gmail.com</p>
+    <h5>Compras</h5>
+    <p>fwdcomprasref@gamil.com</p>
+    <h5>Ventas</h5>
+    <p>fwdshop@gamil.com</p>
 
     </div>
   )
--- reactproyect/src/pages/contact.jsx
+++ reactproyect/src/pages/contact.jsx
@@ -1,9 +1,10 @@
 import Contacto from "../components/contacto/contacto.jsx"
-
+import FormularioContacto from "../components/contacto/FormularioContacto.jsx"
 const Contact = () => {
   return (
     <div>
       <Contacto />
+      <FormularioContacto />
     </div>
   )
 }
